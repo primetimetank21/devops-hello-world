8c18c6ccd0b80f0f832d8a6408b7885fd26ecdc0
Earl Tankard, Jr <theflashtank21@gmail.com>
2022-08-12 02:31:11 -0400

    feat: added new 'library'

--- hello_lib/hello.py
+++ hello_lib/hello.py
@@ -1,5 +1,7 @@
 """This module is for introductory hello world things"""
 
+
 def hello_world():
-    """Prints 'Hello world!'"""
+    """Prints and returns 'Hello world!'"""
     print("Hello world!")
+    return "Hello world!"
